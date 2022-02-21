f08f8e1ebc25094cefd30aac2fb587e45da45cac
Mattias Lij <mattias.lij@teliacompany.com>
2022-02-21 18:58:37 +0100

    Add project files.

---
+++ BandydosWeb/Data/BandydosApiConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace BandydosWeb.Data
+{
+    public class BandydosApiConstants
+    {
+        public const string BaseUri = "https://sportplannerapi.azurewebsites.net";
+
+        public const string EventUri = "api/event";
+
+        public const string UserUri = "api/user";
+
+        public const string Apikey = "OlEfvbvK6YHrBpQa8tIycbz49AR/mgasjZryR3f96TdyXb/ZsEWwaA==";
+
+        public const string UserName = "Matte";
+
+    }
+}
---
+++ BandydosWeb/Data/EventService.cs
@@ -0,0 +1,36 @@
+﻿using SportPlanner.ModelsDto;
+using System.Diagnostics;
+
+namespace BandydosWeb.Data
+{
+    public class EventService
+    {
+        private static readonly HttpClient _httpClient;
+
+        static EventService()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(BandydosApiConstants.BaseUri)
+            };
+            _httpClient.DefaultRequestHeaders.Add("x-functions-key", "OlEfvbvK6YHrBpQa8tIycbz49AR/mgasjZryR3f96TdyXb/ZsEWwaA==");
+        }
+
+        public async Task<IEnumerable<EventDto>> GetEventsAsync()
+        {
+            var uri = new UriBuilder(BandydosApiConstants.BaseUri)
+            {
+                Path = BandydosApiConstants.EventUri,
+            };
+
+            var response = await _httpClient.GetAsync(uri.Uri);
+            if (!response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine($"Failed to get events. Status code: {response.StatusCode}");
+                return new List<EventDto>();
+            }
+
+            return await response.Content.ReadFromJsonAsync<IEnumerable<EventDto>>() ?? new List<EventDto>();
+        }
+    }
+}
